878a8309cb3bc06b63be5728f7eed926f9b38f9c
David Cosme <cosmedavid@hotmail.fr>
2026-01-04 15:50:32 +0100

    Project init - split settings between dev and prod

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,19 +11,25 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Load .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@)qvx@!bwvzarw%!5#bju2xtb2u2nr$&unb@(u*l4x0z$vu2vq'
+SECRET_KEY = os.getenv("SECRET_KEY")
+#SECRET_KEY = 'django-insecure-@)qvx@!bwvzarw%!5#bju2xtb2u2nr$&unb@(u*l4x0z$vu2vq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -58,7 +64,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
